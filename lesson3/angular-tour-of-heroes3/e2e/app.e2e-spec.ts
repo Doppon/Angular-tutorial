4e5b5c79581212285365b202c397c2535a767966
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-26 13:14:06 +0900

    output hero

--- lesson3/angular-tour-of-heroes3/e2e/app.e2e-spec.ts
+++ lesson3/angular-tour-of-heroes3/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularTourOfHeroes3Page } from './app.po';
+import { AppPage } from './app.po';
 
-describe('angular-tour-of-heroes3 App', function() {
-  let page: AngularTourOfHeroes3Page;
+describe('angular-tour-of-heroes3 App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularTourOfHeroes3Page();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- lesson3/angular-tour-of-heroes3/src/app/app.module.ts
+++ lesson3/angular-tour-of-heroes3/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
