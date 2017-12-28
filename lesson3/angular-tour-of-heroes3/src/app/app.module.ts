4afb6c36d97281f54f54e657660fe7e8e4d1ad3a
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2017-12-28 21:02:17 +0900

    clear lesson3

--- lesson3/angular-tour-of-heroes3/src/app/app.module.ts
+++ lesson3/angular-tour-of-heroes3/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
